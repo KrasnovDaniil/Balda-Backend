3ddc29011245bd2dfa10ef2554a667b7f39495bb
Daniil <daniil_krasnov_00@mail.ru>
2021-01-24 00:38:27 +0300

    deleted Frontend folder

---
+++ Frontend/build/precache-manifest.e70be5db5d58114577f6d8d780fcac20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a451d6615079bbc6226477b529153b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ebaf94db10b43775ef7",
+    "url": "/static/css/main.641f62e6.chunk.css"
+  },
+  {
+    "revision": "cff781092b4218bdd798",
+    "url": "/static/js/2.e0469aef.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.e0469aef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ebaf94db10b43775ef7",
+    "url": "/static/js/main.7f88c178.chunk.js"
+  },
+  {
+    "revision": "48a354fb5103cdff3976",
+    "url": "/static/js/runtime-main.12f1707d.js"
+  }
+]);
